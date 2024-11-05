e4df138ff47750cc4adfaaeea251fbbbeafb251f
Jeff Franklin <jefr@redhat.com>
2024-11-05 18:06:33 -0500

    Fix some upgrade breakages

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-mvn package -DskipTests
+./mvnw package -DskipTests
 docker buildx build --platform=linux/amd64 -f src/main/docker/Dockerfile.jvm -t jefrankl/parks-weather .
 docker tag jefrankl/parks-weather quay.io/jefrankl/parks-weather
 docker push quay.io/jefrankl/parks-weather
